5a7dacf49b654bc3a40c1f1bdc38f8e407973a47
Russell <russellmorbey@hotmail.co.uk>
2022-06-16 17:05:24 +0100

    New tests pass.

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+      cb(null, './public/post-uploads/')
+  }
+})
+
+var upload = multer({ storage: storage })
 
 const PostsController = require("../controllers/posts");
 
 router.get("/", PostsController.Index);
-router.post("/", PostsController.Create);
+router.post("/", upload.single('myImage'), PostsController.Create);
 router.post("/create_comment", PostsController.CreateComment);
 router.post("/add_like", PostsController.AddLike);
 
